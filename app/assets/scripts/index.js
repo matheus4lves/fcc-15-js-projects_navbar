7e98ba6fcaa8c36235d1a1391501e21d9e62a00d
Matheus Alves <matheusalves.bmd@gmail.com>
2022-03-09 16:19:27 -0300

    Initial commit

---
+++ app/assets/scripts/index.js
@@ -0,0 +1,11 @@
+import "../styles/styles.css";
+import "@fortawesome/fontawesome-free";
+
+if (module.hot) {
+  module.hot.accept();
+}
+// classList - shows/gets all classes
+// contains - checks classList for specific class
+// add - add class
+// remove - remove class
+// toggle - toggles class
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+let mode = process.env.npm_lifecycle_event;
+
+const path = require("path");
+const postcssPlugins = [require("postcss-import")];
+
+let config = {
+  entry: "./app/assets/scripts/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: postcssPlugins,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
+
+if (mode === "dev") {
+  config.devServer = {
+    host: "local-ip",
+    hot: true,
+    open: {
+      app: {
+        name: "firefox",
+        arguments: ["--private-window"],
+      },
+    },
+    static: {
+      directory: path.join(__dirname, "./app/public"),
+    },
+  };
+}
+
+if (mode === "build") {
+}
+
+module.exports = config;
